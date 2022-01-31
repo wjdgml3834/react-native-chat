edb0deb5858a2ba0090214c54cbc23252271ee02
junghee.kim <wjdgml3834@naver.com>
2022-02-01 08:20:05 +0900

    ✨  Image 컴포넌트 생성 및 로고 추가

--- src/screens/Signin.js
+++ src/screens/Signin.js
@@ -1,14 +1,19 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "styled-components/native";
 import styled from "styled-components/native";
-import { Button } from "../components";
+import { Button, Image } from "../components";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+// 파이어베이스 로고 스토리지 주소
+const LOGO =
+  "https://firebasestorage.googleapis.com/v0/b/rn-chat-e7ab8.appspot.com/o/chat-logo.jpeg?alt=media";
 
 const Signin = ({ navigation }) => {
   const insets = useSafeAreaInsets();
   const theme = useContext(ThemeContext);
   return (
     <Container insets={insets}>
+      <Image url={LOGO} />
       <StyledText>Sign in</StyledText>
       <Button title="Sign up" onPress={() => console.log("sign in")} />
       <Button
--- src/theme.js
+++ src/theme.js
@@ -4,12 +4,19 @@ const Colors = {
   white: "#ffffff",
   black: "#111111",
   main: "#3679fe",
+  grey: "#d5d5d5",
 };
 
 export const theme = {
   background: Colors.white,
   text: Colors.black,
+
+  // 버튼
   btnBackground: Colors.main,
   btnTitle: Colors.white,
   btnTextLink: Colors.main,
+
+  // 이미지
+
+  imgBackground: Colors.grey,
 };
