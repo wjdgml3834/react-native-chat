2646350bd0e73b1d68b90c9d71206bed318c569b
junghee.kim <wjdgml3834@naver.com>
2022-02-06 12:39:13 +0900

    🔧  파이어베이스 업데이트 버전에 맞게 코드 수정

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,16 @@
-import * as firebase from "firebase";
+import { initializeApp } from "firebase/app";
+
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 import config from "../firebase.json";
-
-const app = firebase.initializeApp(config);
-
 // firebase.json 파일들에 있는 값들을 이용해 initializeApp 함수를 이용해 값을 호출해준다.
+
+const app = initializeApp(config);
+
+const auth = getAuth(app);
+
+export const signin = async ({ email, password }) => {
+  const { user } = signInWithEmailAndPassword(auth, email, password);
+
+  return user;
+};
