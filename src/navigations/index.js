62c3e735c08b41f26cf4c42bceda8be131095552
junghee.kim <wjdgml3834@naver.com>
2022-02-08 14:44:12 +0900

    ✨  메인페이지내에 프로필 가져오기

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -11,7 +11,7 @@ const Navigation = () => {
 
   return (
     <NavigationContainer>
-      {!user.uid ? <Main /> : <Auth />}
+      {user.uid ? <Main /> : <Auth />}
       {inProgress && <Spinner />}
     </NavigationContainer>
   );
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,18 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../contexts";
 import styled from "styled-components/native";
-import { Button } from "../components";
+import { Button, Image, Input } from "../components";
+import { getCurrentUser } from "../firebase";
 
 const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.background};
+  justify-content: center;
+  align-items: center;
+  padding: 0 20px;
 `;
 
 const Profile = ({ navigation, route }) => {
   const { setUser } = useContext(UserContext);
+  const user = getCurrentUser();
+
+  const [photo, setPhoto] = useState(user.photo);
   return (
     <Container>
-      <Button title="signout" onPress={() => setUser({})} />
+      <Image showButton url={photo} />
+      <Input label="Name" />
+      <Input label="Email" />
+      <Button title="Sign out" onPress={() => setUser({})} />
     </Container>
   );
 };
